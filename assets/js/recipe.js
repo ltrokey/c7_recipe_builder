0d1bb9b18b46490b0db14cd4d545cedf8115065e
Lacey Trokey <trokeyln@gmail.com>
2023-09-27 20:35:02 -0600

    added api function for recipe

--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -1,5 +1,22 @@
 $(document).ready(function () {
 
+    var apiKey = 'bf4058784ea84f3f89a280160f3d1ff3'
+    var ingredient1 = 'apple'
+
+
+    function getIngredient() {
+        var requestUrlLocation = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredient1}&appid=${apiKey}`
+
+        fetch(requestUrlLocation)
+          .then(function(response) {
+              return response.json()
+          })
+          .then(function(data) {
+            console.log(data)
+          })
+      }
+
+      getIngredient()
     // all code goes inside this function
 
 
