48dda22f508165fd1770bfd25db45b54915b23f1
Heet <85766553+Heet-09@users.noreply.github.com>
2023-09-28 11:22:11 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,22 @@
 $(document).ready(function () {
+    $('#searchButton').on('click', function () {
+        var ingredient = $('#ingredientInput').val();
+        var searchType = $('#searchType').val();
 
-    // all code goes inside this function
+        if (searchType === 'recipe') {
+            // Call a function to fetch recipes based on the ingredient
+            getRecipes(ingredient);
+        } else if (searchType === 'nutrient') {
+            // Call a function to fetch nutrient info based on the ingredient
+            getNutrientInfo(ingredient);
+        }
+    });
 
+    function getRecipes(ingredient) {
+        // Implement recipe fetching logic here
+    }
 
-})
+    function getNutrientInfo(ingredient) {
+        // Implement nutrient info fetching logic here
+    }
+});
