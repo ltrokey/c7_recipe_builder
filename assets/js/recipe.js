2e6be060582e28b0b189b365b20ff4723aa2788c
Lacey Trokey <trokeyln@gmail.com>
2023-09-29 15:05:26 -0600

    reorganized ingredient function for clarity and added comments

--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
     var userInput = ''
     var urlIngredients = ''
 
+  // User Search Parameters
     function getParams() {
       var searchParams = new URLSearchParams(document.location.search)
       userInput = searchParams.get("q")
@@ -17,6 +18,7 @@ $(document).ready(function() {
 
     getParams()
 
+  // Reipe Preview Cards
     function getIngredientRecipes() {
       var requestUrlIngredient = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${urlIngredients}&apiKey=${apiKey}`
 
@@ -26,35 +28,28 @@ $(document).ready(function() {
         })
         .then(function(data) {
           for (var i = 0; i < data.length; i++) {
-            var recipeName = $('<h2>').text(data[i].title)
+
+            // Image Column
+            var imageColumn = $('<div>').addClass('col-md-4')
             var recipeImage = $('<img>').addClass('img-fluid').attr('src', recipeImageUrl)
             var recipeImageUrl = data[i].image
-            var recipeId = $('<p>').addClass('text-right').text('Recipe Id: ' + data[i].id)
-            var recipeLikes = $('<p>').text('Likes: ' + data[i].likes)
-
-            var container = $('<div>').addClass('row mb-3 border border-secondary')
-
-            var imageColumn = $('<div>').addClass('col-md-4')
-
-            var detailsColumn = $('<div>').addClass('col-md-8')
-
-            detailsColumn.append(recipeId, recipeName, recipeLikes)
 
             imageColumn.append(recipeImage)
 
+            // Details Column
+            var detailsColumn = $('<div>').addClass('col-md-8')
+            var recipeName = $('<h2>').text(data[i].title)
+            var recipeId = $('<p>').addClass('text-right').text('Recipe Id: ' + data[i].id)
+            var recipeLikes = $('<p>').text('Likes: ' + data[i].likes)
+            var viewBtn = $('<button>').text("View")
+
+            detailsColumn.append(recipeId, recipeName, recipeLikes, viewBtn)
+
+            //Container per Recipe Preview (combines colums)
+            var container = $('<div>').addClass('row m-3 border border-secondary')
             container.append(imageColumn,detailsColumn)
 
-
             $('#recipePreviewContainer').append(container)
-            // console.log(data[i])
-            // // Recipe Id
-            // console.log(data[i].id)
-            // // Recipe Image
-            // console.log(data[i].image)
-            // // Recipe Name
-            // console.log(data[i].title)
-            // // Recipe Likes
-            // console.log(data[i].likes)
           }
           // Place Holder Id - First Item (need to add event listener?)
           // getRecipe(data[3].id)
@@ -63,35 +58,36 @@ $(document).ready(function() {
           console.error("An error occurred:", error)
       })
     }
-      // getIngredientRecipes()
 
+    // Recipe Card
+    function getRecipe(recipeId) {
+      var requestUrlRecipeIngredients = `https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${apiKey}`
 
-    // function getRecipe(recipeId) {
-    //   var requestUrlRecipeIngredients = `https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${apiKey}`
+      fetch(requestUrlRecipeIngredients)
+      .then(function(response) {
+          return response.json()
+      })
+      .then(function(data) {
+        for (var i = 0; i < data.extendedIngredients.length; i++) {
+        console.log(data)
+        // Ingredient Name
+        console.log(data.extendedIngredients[i].nameClean)
+        // Ingredient Amount
+        console.log(data.extendedIngredients[i].measures.us.amount)
+        // Ingredient Measurement
+        console.log(data.extendedIngredients[i].measures.us.unitShort)
 
-    //   fetch(requestUrlRecipeIngredients)
-    //   .then(function(response) {
-    //       return response.json()
-    //   })
-    //   .then(function(data) {
-    //     for (var i = 0; i < data.extendedIngredients.length; i++) {
-    //     console.log(data)
-    //     // Ingredient Name
-    //     console.log(data.extendedIngredients[i].nameClean)
-    //     // Ingredient Amount
-    //     console.log(data.extendedIngredients[i].measures.us.amount)
-    //     // Ingredient Measurement
-    //     console.log(data.extendedIngredients[i].measures.us.unitShort)
+        // var favoriteBtn = $('<button>').text("Favorite")
 
-    //     }
-    //     // Recipe Instruction (add modal for note if unavailable)
-    //     console.log(data.instructions)
-    //   })
-    //   .catch(function(error) {
-    //     console.error("An error occurred:", error)
-    // })
-    // }
-    //   getRecipe()
+        }
+        // Recipe Instruction (add modal for note if unavailable)
+        console.log(data.instructions)
+      })
+      .catch(function(error) {
+        console.error("An error occurred:", error)
+    })
+    }
+      getRecipe()
 
 })
 
