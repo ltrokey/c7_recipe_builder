e7b748af29919edb6e572940b10899352bb105f8
Lacey Trokey <trokeyln@gmail.com>
2023-10-02 14:47:26 -0600

    added svg to add icon to favorite, add error message text for recipe instructions

--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -113,6 +113,29 @@ $(document).ready(function() {
           saveFavoriteRecipe(recipeId, recipeName)
         })
 
+        // Ask Eric - Trying to use Icon
+        // var svgElement = $('<svg>').attr({
+        //   xmlns: 'http://www.w3.org/2000/svg',
+        //   height: '1em',
+        //   viewBox: '0 0 576 512',
+        // })
+
+        // var pathElement = $('<path>').attr('d', 'M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z')
+
+        // svgElement.append(pathElement)
+
+        // var btnIcon = $('<i>').addClass('fa-solid fa-star')
+
+        // btnIcon.append(svgElement)
+
+        // var favoriteBtn = $('<button>').addClass('col-4 col-md-2 ms-auto m-2')
+
+        // favoriteBtn.append(btnIcon)
+
+        // $(favoriteBtn).on('click', function() {
+        //   saveFavoriteRecipe(recipeId, recipeName)
+        // })
+
         // Ingredient Container
         containerDetails.append(ingredientTitle, ingredientsListEl, ingredientsList)
 
@@ -125,8 +148,11 @@ $(document).ready(function() {
         var listTitle = $('<h2>').addClass('text-center').text(recipeName)
 
         // Recipe Instuctions
+
+        var instructionText = data.instructions || "Oops! 🙁 This information is currently unavailable. Please check again later."
+
         var instructionTitle = $('<h3>').addClass('mt-4 mb-2').text('Instructions')
-        var recipeInstructions = $('<div>').html(data.instructions)
+        var recipeInstructions = $('<div>').html(instructionText)
 
         var cardContainer = $('<div>').addClass('row m-3 border border-secondary')
         cardContainer.append(favoriteBtn,imageColumn, listTitle, containerDetails, instructionTitle, recipeInstructions)
